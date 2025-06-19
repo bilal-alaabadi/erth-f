47a97cadbb25472e59632b0f526c6c02faaa8f58
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-06-19 17:03:00 +0400

    dd

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 export const getBaseUrl = () => {
-    return 'https://erth-b.vercel.app';//رابط الباك اند بدل 5000
+    return 'https://www.erthe.online';//رابط الباك اند بدل 5000
 }
 // export const getBaseUrl = () => {
 //     return 'https://genuine-b.vercel.app';//رابط الباك اند بدل 5000 رجع
