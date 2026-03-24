ba7c036d7e70f5798b492799012151be4745bdaf
bilal-alaabadi <mdbilalnizwa@gmail.com>
2026-03-24 13:29:29 +0400

    وصف التغييرات هنا

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,6 +1,7 @@
 export const getBaseUrl = () => {
     return 'https://www.erthe.online';//رابط الباك اند بدل 5000
 }
+
 // export const getBaseUrl = () => {
 //     return 'https://genuine-b.vercel.app';//رابط الباك اند بدل 5000 رجع
 // }
